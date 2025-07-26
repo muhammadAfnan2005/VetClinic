33a1aa8a56692a0a2d7bb99fc8afb24a50bf3ef9
Afnan <afnanmuhammad394@gmail.com>
2025-07-26 20:57:04 +0500

    Enter sql 50%

--- interface.py
+++ interface.py
@@ -15,7 +15,8 @@ def menu():
               1- Fetch data.
               2- Enter data.
               3- Update data.
-              4- Delete data.""")
+              4- Delete data.
+              """)
 
 def fetch_menu():
         print("""Which Data you want to fetch?
@@ -24,7 +25,8 @@ def fetch_menu():
               3- Appointments.
               4- Medicines.
               5- Bills.
-              6- Vaccinations.""")
+              6- Vaccinations.
+              7- Owner.""")
         
 def input_menu():
         print("""Which data you want to Enter?
@@ -33,7 +35,8 @@ def input_menu():
               3- Appointments.
               4- Medicines.
               5- Bills.
-              6- Vaccinations.""")
+              6- Vaccinations.
+              7- Owner.""")
         
 def update_menu():
         print("""Which data you want to Update?
@@ -42,7 +45,8 @@ def update_menu():
               3- Appointments.
               4- Medicines.
               5- Bills.
-              6- Vaccinations.""")
+              6- Vaccinations.
+              7- Owner.""")
         
 def delete_menu():
         print("""Which data you want to Delete?
@@ -65,16 +69,40 @@ if option == '1':
         fetch = input("Enter option = ")
         if fetch== '1':
                 print("Pets info")
+                cur.execute('''SELECT * FROM Pets''')
+                for row in cur.fetchall():
+                        print(row)
         elif fetch=='2':
                 print("Treatment info")
+                cur.execute('''SELECT * FROM Treatments''')
+                for row in cur.fetchall():
+                        print(row)
         elif fetch == '3':
                 print("Appointment info")
+                cur.execute('''SELECT * FROM Appointments''')
+                for row in cur.fetchall():
+                        print(row)
         elif fetch == '4':
                 print("Meddicine info")
+                cur.execute('''SELECT * FROM Medicines''')
+                for row in cur.fetchall():
+                        print(row)
         elif fetch == '5':
                 print("Bills")
+                cur.execute('''SELECT * FROM Bills''')
+                for row in cur.fetchall():
+                        print(row)
         elif fetch == '6':
                 print("Vaccination info")
+                cur.execute('''SELECT * FROM Vaccinations''')
+                for row in cur.fetchall():
+                        print(row)
+        elif fetch =='7':
+                print("Owner info")
+                print("Vaccination info")
+                cur.execute('''SELECT * FROM Owner''')
+                for row in cur.fetchall():
+                        print(row)
         else:
                 print("Invalid input")
 
@@ -82,17 +110,61 @@ elif option == '2':
         input_menu()
         inpt = input("Enter option = ")
         if inpt== '1':
-                print("Pets info")
+                print("For entering a new pet, Owner details must antered first!")
+                name = input("Enter pet name = ")
+                specie = input("Enter pet specie = ")
+                breed = input("Enter Breed = ")
+                age = int(input("Enter age = "))
+                gender = input("Enter pet gender = ")
+                owner = input("Enter Owner ID (same as Owner table) = ")
+                cur.execute('''INSERT INTO Pets(petName,species,breed,age,gender,ownerID)
+                            VALUES(?,?,?,?,?,?)''',(name,specie,breed,age,gender,owner))
+                conn.commit()
         elif inpt=='2':
-                print("Treatment info")
+                appID = int(input("Enter appointment ID = "))
+                diagnos = input("Enter diagnpsis result = ")
+                med = input("Enter Medicine = ")
+                dos = input("Enter dosage of medicine = ")
+                advice = input("Enter Vet's Advice = ")
+                cur.execute('''INSERT INTO Treatments(appointmentID,diagnosis,medicines,dosage,advice)
+                           VALUES(?,?,?,?,?) ''',(appID,diagnos,med,dos,advice))
+                conn.commit()
+
         elif inpt == '3':
                 print("Appointment info")
+                pet = input("Enter pet ID = ")
+                vet = input("Enter Vet ID = ")
+                time = input("Enter time = ")
+                cause = input("Enter disease = ")
+                cur.execute('''INSERT INTO Appointment(petID,vetID,Time,cause)
+                            VALUES(?,?,?,?)''',(pet,vet,time,cause))
+                conn.commit()
+
         elif inpt == '4':
                 print("Meddicine info")
+                name = input("Enter Medicine name = ")
+                man = input("Enter manufacturer = ")
+                dose = input("Enter dosage = ")
+                unit = input("Enter unit = ")
+                descrip = input("Enter Description = ")
+                cur.execute('''INSERT INTO Medicines(medicineName,manufacture,dose,unit,description)
+                            VALUES(?,?,?,?,?)''',(name,man,dose,unit,descrip))
+                conn.commit()
         elif inpt == '5':
                 print("Bills")
+                treatID = input('''Enter Treatment ID''')
+                date = input('''Enter bill date = ''')
+                amount = int(input('''Enter bill amount = '''))
+                paystatus = input('''Enter payment status = ''')
+                paymethod = input('''Enter payment method = ''')
+                cur.execute('''INSERT INTO Bills(treatmentID,billDate,amount,paymentstatus,paymentmethod)
+                            VALUES(?,?,?,?,?)''',(treatID,date,amount,paystatus,paymethod))
+                conn.commit()
         elif inpt == '6':
                 print("Vaccination info")
+        elif input == '7':
+                print("Enter Owner's data")
+
         else:
                 print("Invalid input")
 
